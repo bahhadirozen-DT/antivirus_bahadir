1cca966d0798016cdb31b2fb79d95622f81ff95b
bahhadirozen-DT <bahhadirozen@gmail.com>
2026-06-11 09:42:29 +0300

    Update app.py

--- antivirus-scanner/app.py
+++ antivirus-scanner/app.py
@@ -300,5 +300,4 @@ def manage_quarantine():
         
     return jsonify({"success": False, "message": "İşlem tamamlanamadı."})
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == '__main
